513350ee2327968111f7ca605f5ee12e35cfe02a
michal200456 <michal200456@gmail.com>
2025-05-26 17:09:55 +0200

    Add files via upload

---
+++ Main.cpp
@@ -0,0 +1,696 @@
+﻿#include <vector>
+#include <algorithm>
+#include <functional> 
+#include <memory>
+#include <cstdlib>
+#include <cmath>
+#include <ctime>
+
+#include <raylib.h>
+#include <raymath.h>
+
+// --- UTILS ---
+namespace Utils {
+	inline static float RandomFloat(float min, float max) {
+		return min + static_cast<float>(rand()) / RAND_MAX * (max - min);
+	}
+}
+
+// --- TRANSFORM, PHYSICS, LIFETIME, RENDERABLE ---
+struct TransformA {
+	Vector2 position{};
+	float rotation{};
+};
+
+struct Physics {
+	Vector2 velocity{};
+	float rotationSpeed{};
+};
+
+struct Renderable {
+	enum Size { SMALL = 1, MEDIUM = 2, LARGE = 4 } size = SMALL;
+};
+
+// --- RENDERER ---
+class Renderer {
+public:
+	static Renderer& Instance() {
+		static Renderer inst;
+		return inst;
+	}
+
+	void Init(int w, int h, const char* title) {
+		InitWindow(w, h, title);
+		SetTargetFPS(60);
+		screenW = w;
+		screenH = h;
+	}
+
+	void Begin() {
+		BeginDrawing();
+		ClearBackground(BLACK);
+	}
+
+	void End() {
+		EndDrawing();
+	}
+
+	void DrawPoly(const Vector2& pos, int sides, float radius, float rot) {
+		DrawPolyLines(pos, sides, radius, rot, WHITE);
+	}
+
+	int Width() const {
+		return screenW;
+	}
+
+	int Height() const {
+		return screenH;
+	}
+
+private:
+	Renderer() = default;
+	
+	int screenW{};
+	int screenH{};
+};
+
+// Shape selector
+enum class AsteroidShape { TRIANGLE = 3, SQUARE = 4, PENTAGON = 5, VOLATILE = 6, CHUNK = 7, RANDOM = 0 };
+
+// --- ASTEROID HIERARCHY ---
+
+class Asteroid {
+public:
+	Asteroid(int screenW, int screenH, AsteroidShape type) {
+		init(screenW, screenH, type);
+	}
+	virtual ~Asteroid() = default;
+
+	bool Update(float dt) {
+		transform.position = Vector2Add(transform.position, Vector2Scale(physics.velocity, dt));
+		transform.rotation += physics.rotationSpeed * dt;
+		if (transform.position.x < -GetRadius() || transform.position.x > Renderer::Instance().Width() + GetRadius() ||
+			transform.position.y < -GetRadius() || transform.position.y > Renderer::Instance().Height() + GetRadius())
+			return false;
+		return true;
+	}
+	virtual void Draw() const = 0;
+
+	Vector2 GetPosition() const {
+		return transform.position;
+	}
+
+	float constexpr GetRadius() const {
+		return 16.f * (float)render.size;
+	}
+
+	int GetDamage() const {
+		return baseDamage * static_cast<int>(render.size);
+	}
+
+	int GetSize() const {
+		return static_cast<int>(render.size);
+	}
+
+	AsteroidShape GetType() {
+		return type;
+	}
+	
+protected:
+	void init(int screenW, int screenH, AsteroidShape AsteroidType) {
+		type = AsteroidType;
+		// Choose size
+		render.size = static_cast<Renderable::Size>(1 << GetRandomValue(0, 2));
+
+		// Spawn at random edge
+		switch (GetRandomValue(0, 3)) {
+		case 0:
+			transform.position = { Utils::RandomFloat(0, screenW), -GetRadius() };
+			break;
+		case 1:
+			transform.position = { screenW + GetRadius(), Utils::RandomFloat(0, screenH) };
+			break;
+		case 2:
+			transform.position = { Utils::RandomFloat(0, screenW), screenH + GetRadius() };
+			break;
+		default:
+			transform.position = { -GetRadius(), Utils::RandomFloat(0, screenH) };
+			break;
+		}
+
+		// Aim towards center with jitter
+		float maxOff = fminf(screenW, screenH) * 0.1f;
+		float ang = Utils::RandomFloat(0, 2 * PI);
+		float rad = Utils::RandomFloat(0, maxOff);
+		Vector2 center = {
+										 screenW * 0.5f + cosf(ang) * rad,
+										 screenH * 0.5f + sinf(ang) * rad
+		};
+
+		Vector2 dir = Vector2Normalize(Vector2Subtract(center, transform.position));
+		physics.velocity = Vector2Scale(dir, Utils::RandomFloat(SPEED_MIN, SPEED_MAX));
+		physics.rotationSpeed = Utils::RandomFloat(ROT_MIN, ROT_MAX);
+
+		transform.rotation = Utils::RandomFloat(0, 360);
+	}
+
+	TransformA transform;
+	Physics    physics;
+	Renderable render;
+	AsteroidShape type;
+
+	int baseDamage = 0;
+	static constexpr float LIFE = 10.f;
+	static constexpr float SPEED_MIN = 125.f;
+	static constexpr float SPEED_MAX = 250.f;
+	static constexpr float ROT_MIN = 50.f;
+	static constexpr float ROT_MAX = 240.f;
+};
+
+class TriangleAsteroid : public Asteroid {
+public:
+	TriangleAsteroid(int w, int h) : Asteroid(w, h, AsteroidShape::TRIANGLE) { baseDamage = 5; }
+	void Draw() const override {
+		Renderer::Instance().DrawPoly(transform.position, 3, GetRadius(), transform.rotation);
+	}
+};
+class SquareAsteroid : public Asteroid {
+public:
+	SquareAsteroid(int w, int h) : Asteroid(w, h, AsteroidShape::SQUARE) { baseDamage = 10; }
+	void Draw() const override {
+		Renderer::Instance().DrawPoly(transform.position, 4, GetRadius(), transform.rotation);
+	}
+};
+class PentagonAsteroid : public Asteroid {
+public:
+	PentagonAsteroid(int w, int h) : Asteroid(w, h, AsteroidShape::PENTAGON) { baseDamage = 15; }
+	void Draw() const override {
+		Renderer::Instance().DrawPoly(transform.position, 5, GetRadius(), transform.rotation);
+	}
+};
+class VolatileAsteroid : public Asteroid {
+public:
+	VolatileAsteroid(int w, int h) : Asteroid(w, h, AsteroidShape::VOLATILE) { baseDamage = 30; }
+	void Draw() const override {
+		Renderer::Instance().DrawPoly(transform.position, 8, GetRadius(), transform.rotation);
+	}
+};
+class Chunk : public Asteroid {
+public:
+	Chunk(int w, int h, int n, Vector2 pos) : Asteroid(w, h, AsteroidShape::CHUNK) {
+		baseDamage = 5;
+		render.size=static_cast<Renderable::Size>(1);
+		transform.position = pos;
+		float angle = static_cast<float>(n) / 8.f * 360.f;
+		transform.rotation = angle;
+		Vector2 vel = { 300, 0 };
+		physics.velocity = Vector2Rotate(vel, angle);
+	}
+	void Draw() const override {
+		Renderer::Instance().DrawPoly(transform.position, 3, GetRadius(), transform.rotation);
+	}
+};
+
+
+
+// Factory
+static inline std::unique_ptr<Asteroid> MakeAsteroid(int w, int h, AsteroidShape shape) {
+	switch (shape) {
+	case AsteroidShape::TRIANGLE:
+		return std::make_unique<TriangleAsteroid>(w, h);
+	case AsteroidShape::SQUARE:
+		return std::make_unique<SquareAsteroid>(w, h);
+	case AsteroidShape::PENTAGON:
+		return std::make_unique<PentagonAsteroid>(w, h);
+	case AsteroidShape::VOLATILE:
+		return std::make_unique<VolatileAsteroid>(w, h);
+	default: {
+		return MakeAsteroid(w, h, static_cast<AsteroidShape>(3 + GetRandomValue(0, 3)));
+	}
+	}
+}
+
+// --- PROJECTILE HIERARCHY ---
+enum class WeaponType { LASER, BULLET, MISSILE, COUNT };
+class Projectile {
+public:
+	Vector2 toClosestAsteroid = Vector2Zero();
+	Projectile(Vector2 pos, Vector2 vel, int dmg, WeaponType wt)
+	{
+		transform.position = pos;
+		physics.velocity = vel;
+		baseDamage = dmg;
+		type = wt;
+	}
+	bool Update(float dt) {
+		transform.position = Vector2Add(transform.position, Vector2Scale(physics.velocity, dt));
+		
+		if (transform.position.x < 0 ||
+			transform.position.x > Renderer::Instance().Width() ||
+			transform.position.y < 0 ||
+			transform.position.y > Renderer::Instance().Height())
+		{
+			return true;
+		}
+		return false;
+	}
+	bool UpdateMissile(float dt) {
+		if (!Vector2Equals(toClosestAsteroid, Vector2Zero())) {
+			auto angle = Vector2Angle(physics.velocity, toClosestAsteroid);
+			auto normalAcceleration = Vector2Rotate(physics.velocity, DEG2RAD * 90);
+			if (Vector2DotProduct(normalAcceleration, toClosestAsteroid) < 0.f) {
+				Vector2Scale(normalAcceleration, -1);
+			}
+			
+			auto v = Vector2Length(physics.velocity);
+			auto a = v * v * angle * RAD2DEG / 3600 / 2;
+			normalAcceleration = Vector2Scale(Vector2Normalize(normalAcceleration), a);
+			physics.velocity = Vector2Add(physics.velocity, Vector2Scale(normalAcceleration, dt));
+			physics.velocity = Vector2Scale(Vector2Normalize(physics.velocity), v);
+
+			Vector2 OX = { 0, -1 };
+			transform.rotation = RAD2DEG * Vector2Angle(OX, physics.velocity);
+		}
+		
+		transform.position = Vector2Add(transform.position, Vector2Scale(physics.velocity, dt));
+		
+		if (transform.position.x < 0 ||
+			transform.position.x > Renderer::Instance().Width() ||
+			transform.position.y < 0 ||
+			transform.position.y > Renderer::Instance().Height())
+		{
+			return true;
+		}
+		return false;
+	}
+	void Draw() const {
+		switch (type) {
+		case WeaponType::BULLET:
+			DrawCircleV(transform.position, 5.f, WHITE);
+			break;
+		default:
+			static constexpr float LASER_LENGTH = 30.f;
+			Rectangle lr = { transform.position.x - 2.f, transform.position.y - LASER_LENGTH, 4.f, LASER_LENGTH };
+			DrawRectangleRec(lr, RED);
+			break;
+		}
+	}
+	void Draw(Texture2D texture) const {
+		static constexpr float scale = 0.1f;
+			Vector2 dstPos = {
+										 transform.position.x - (texture.width * scale) * 0.5f,
+										 transform.position.y - (texture.height * scale) * 0.5f
+			};
+			DrawTextureEx(texture, dstPos, transform.rotation, scale, WHITE);
+	}
+	Vector2 GetPosition() const {
+		return transform.position;
+	}
+
+	float GetRadius() const {
+		switch (type)
+		{
+		case WeaponType::BULLET:
+			return 5.f;
+		case WeaponType::MISSILE:
+			return 5.f;
+		default:
+			return 2.f;
+		}
+	}
+
+	int GetDamage() const {
+		return baseDamage;
+	}
+
+	WeaponType GetType() const {
+		return type;
+	}
+
+private:
+	TransformA transform;
+	Physics    physics;
+	int        baseDamage;
+	WeaponType type;
+};
+
+inline static Projectile MakeProjectile(WeaponType wt,
+	const Vector2 pos,
+	float speed)
+{
+	Vector2 vel{ 0, -speed };
+	switch (wt) {
+	case WeaponType::BULLET:
+		return Projectile(pos, vel, 10, wt);
+	case WeaponType::MISSILE:
+		return Projectile(pos, vel, 10, wt);
+	default:
+		return Projectile(pos, vel, 20, wt);
+	}
+}
+
+// --- SHIP HIERARCHY ---
+class Ship {
+public:
+	Ship(int screenW, int screenH) {
+		transform.position = {
+												 screenW * 0.5f,
+												 screenH * 0.5f
+		};
+		hp = 100;
+		speed = 250.f;
+		alive = true;
+
+		// per-weapon fire rate & spacing
+		fireRateLaser = 4.f; // shots/sec
+		fireRateBullet = 8.f;
+		fireRateMissile = 2.f;
+		spacingLaser = 300.f; // px between lasers
+		spacingBullet = 80.f;
+		spacingMissile = 200.f;
+	}
+	virtual ~Ship() = default;
+	virtual void Update(float dt) = 0;
+	virtual void Draw() const = 0;
+
+	void TakeDamage(int dmg) {
+		if (!alive) return;
+		hp -= dmg;
+		if (hp <= 0) alive = false;
+	}
+
+	bool IsAlive() const {
+		return alive;
+	}
+
+	Vector2 GetPosition() const {
+		return transform.position;
+	}
+
+	virtual float GetRadius() const = 0;
+
+	int GetHP() const {
+		return hp;
+	}
+
+	float GetFireRate(WeaponType wt) const {
+		switch (wt) {
+		case WeaponType::BULLET:
+			return fireRateBullet;
+		case WeaponType::MISSILE:
+			return fireRateMissile;
+		default:
+			return fireRateLaser;
+		}
+	}
+
+	float GetSpacing(WeaponType wt) const {
+		switch (wt) {
+		case WeaponType::BULLET:
+			return spacingBullet;
+		case WeaponType::MISSILE:
+			return spacingMissile;
+		default:
+			return spacingLaser;
+		}
+	}
+
+protected:
+	TransformA transform;
+	int        hp;
+	float      speed;
+	bool       alive;
+	float      fireRateLaser;
+	float      fireRateBullet;
+	float      fireRateMissile;
+	float      spacingLaser;
+	float      spacingBullet;
+	float      spacingMissile;
+};
+
+class PlayerShip :public Ship {
+public:
+	PlayerShip(int w, int h) : Ship(w, h) {
+		texture = LoadTexture("spaceship1.png");
+		GenTextureMipmaps(&texture);                                                        // Generate GPU mipmaps for a texture
+		SetTextureFilter(texture, 2);
+		scale = 0.25f;
+	}
+	~PlayerShip() {
+		UnloadTexture(texture);
+	}
+
+	void Update(float dt) override {
+		if (alive) {
+			if (IsKeyDown(KEY_W)) transform.position.y -= speed * dt;
+			if (IsKeyDown(KEY_S)) transform.position.y += speed * dt;
+			if (IsKeyDown(KEY_A)) transform.position.x -= speed * dt;
+			if (IsKeyDown(KEY_D)) transform.position.x += speed * dt;
+		}
+		else {
+			transform.position.y += speed * dt;
+		}
+	}
+
+	void Draw() const override {
+		if (!alive && fmodf(GetTime(), 0.4f) > 0.2f) return;
+		Vector2 dstPos = {
+										 transform.position.x - (texture.width * scale) * 0.5f,
+										 transform.position.y - (texture.height * scale) * 0.5f
+		};
+		DrawTextureEx(texture, dstPos, 0.0f, scale, WHITE);
+	}
+
+	float GetRadius() const override {
+		return (texture.width * scale) * 0.5f;
+	}
+
+private:
+	Texture2D texture;
+	float     scale;
+};
+
+// --- APPLICATION ---
+class Application {
+public:
+	static Application& Instance() {
+		static Application inst;
+		return inst;
+	}
+
+	void Run() {
+		srand(static_cast<unsigned>(time(nullptr)));
+		Renderer::Instance().Init(C_WIDTH, C_HEIGHT, "Asteroids OOP");
+
+		auto player = std::make_unique<PlayerShip>(C_WIDTH, C_HEIGHT);
+
+		MissileTexture = LoadTexture("missile.png");
+		GenTextureMipmaps(&MissileTexture);
+		SetTextureFilter(MissileTexture, 2);
+
+		float spawnTimer = 0.f;
+		float spawnInterval = Utils::RandomFloat(C_SPAWN_MIN, C_SPAWN_MAX);
+		WeaponType currentWeapon = WeaponType::LASER;
+		float shotTimer = 0.f;
+
+		while (!WindowShouldClose()) {
+			float dt = GetFrameTime();
+			spawnTimer += dt;
+
+			// Update player
+			player->Update(dt);
+
+			// Restart logic
+			if (!player->IsAlive() && IsKeyPressed(KEY_R)) {
+				player = std::make_unique<PlayerShip>(C_WIDTH, C_HEIGHT);
+				asteroids.clear();
+				projectiles.clear();
+				spawnTimer = 0.f;
+				spawnInterval = Utils::RandomFloat(C_SPAWN_MIN, C_SPAWN_MAX);
+			}
+			// Asteroid shape switch
+			if (IsKeyPressed(KEY_ONE)) {
+				currentShape = AsteroidShape::TRIANGLE;
+			}
+			if (IsKeyPressed(KEY_TWO)) {
+				currentShape = AsteroidShape::SQUARE;
+			}
+			if (IsKeyPressed(KEY_THREE)) {
+				currentShape = AsteroidShape::PENTAGON;
+			}
+			if (IsKeyPressed(KEY_FOUR)) {
+				currentShape = AsteroidShape::RANDOM;
+			}
+
+			// Weapon switch
+			if (IsKeyPressed(KEY_TAB)) {
+				currentWeapon = static_cast<WeaponType>((static_cast<int>(currentWeapon) + 1) % static_cast<int>(WeaponType::COUNT));
+			}
+
+			// Shooting
+			{
+				if (player->IsAlive() && IsKeyDown(KEY_SPACE)) {
+					shotTimer += dt;
+					float interval = 1.f / player->GetFireRate(currentWeapon);
+					float projSpeed = player->GetSpacing(currentWeapon) * player->GetFireRate(currentWeapon);
+
+					while (shotTimer >= interval) {
+						Vector2 p = player->GetPosition();
+						p.y -= player->GetRadius();
+						projectiles.push_back(MakeProjectile(currentWeapon, p, projSpeed));
+						shotTimer -= interval;
+					}
+				}
+				else {
+					float maxInterval = 1.f / player->GetFireRate(currentWeapon);
+
+					if (shotTimer > maxInterval) {
+						shotTimer = fmodf(shotTimer, maxInterval);
+					}
+				}
+			}
+
+			// Spawn asteroids
+			if (spawnTimer >= spawnInterval && asteroids.size() < MAX_AST) {
+				asteroids.push_back(MakeAsteroid(C_WIDTH, C_HEIGHT, currentShape));
+				spawnTimer = 0.f;
+				spawnInterval = Utils::RandomFloat(C_SPAWN_MIN, C_SPAWN_MAX);
+			}
+
+			for (auto j = projectiles.begin(); j != projectiles.end(); ++j) {
+				if ((*j).GetType() != WeaponType::MISSILE) {
+					continue;
+				}
+				float minDistance = FLT_MAX;
+				Vector2 minDelta = Vector2Zero();
+				for (auto i = asteroids.begin(); i != asteroids.end(); ++i) {
+					auto delta = Vector2Subtract((**i).GetPosition(), (*j).GetPosition());
+					auto deltaL = Vector2Length(delta);
+					if (deltaL < minDistance) {
+						minDistance = deltaL;
+						minDelta = delta;
+					}
+				}
+				(*j).toClosestAsteroid = minDelta;
+			}
+
+			// Update projectiles - check if in boundries and move them forward
+			{
+
+				auto projectile_to_remove = std::remove_if(projectiles.begin(), projectiles.end(),
+					[dt](auto& projectile) {
+						if (projectile.GetType() == WeaponType::MISSILE) {
+							return projectile.UpdateMissile(dt);
+						}
+						else {
+							return projectile.Update(dt);
+						}
+					});
+				projectiles.erase(projectile_to_remove, projectiles.end());
+			}
+
+			// Projectile-Asteroid collisions O(n^2)
+			for (auto pit = projectiles.begin(); pit != projectiles.end();) {
+				bool removed = false;
+
+				for (auto ait = asteroids.begin(); ait != asteroids.end(); ++ait) {
+					float dist = Vector2Distance((*pit).GetPosition(), (*ait)->GetPosition());
+					if (dist < (*pit).GetRadius() + (*ait)->GetRadius()) {
+						if ((*ait)->GetType() == AsteroidShape::VOLATILE) {
+							for (int i = 0;i < 8;i++) {
+								asteroids.push_back(std::make_unique<Chunk>(C_WIDTH, C_HEIGHT,i,(*ait)->GetPosition()));
+							}
+						}
+						ait = asteroids.erase(ait);
+						pit = projectiles.erase(pit);
+						removed = true;
+						break;
+					}
+				}
+				if (!removed) {
+					++pit;
+				}
+			}
+
+			// Asteroid-Ship collisions
+			{
+				auto remove_collision =
+					[&player, dt](auto& asteroid_ptr_like) -> bool {
+					if (player->IsAlive()) {
+						float dist = Vector2Distance(player->GetPosition(), asteroid_ptr_like->GetPosition());
+
+						if (dist < player->GetRadius() + asteroid_ptr_like->GetRadius()) {
+							player->TakeDamage(asteroid_ptr_like->GetDamage());
+							return true; // Mark asteroid for removal due to collision
+						}
+					}
+					if (!asteroid_ptr_like->Update(dt)) {
+						return true;
+					}
+					return false; // Keep the asteroid
+					};
+				auto asteroid_to_remove = std::remove_if(asteroids.begin(), asteroids.end(), remove_collision);
+				asteroids.erase(asteroid_to_remove, asteroids.end());
+			}
+
+			// Render everything
+			{
+				Renderer::Instance().Begin();
+
+				DrawText(TextFormat("HP: %d", player->GetHP()),
+					10, 10, 20, GREEN);
+				const char* weaponNames[] = { "LASER", "BULLET", "MISSILE" };
+				const char* weaponName = weaponNames[static_cast<int>(currentWeapon)];
+				DrawText(TextFormat("Weapon: %s", weaponName),
+					10, 40, 20, BLUE);
+
+				for (const auto& projPtr : projectiles) {
+					if (projPtr.GetType() == WeaponType::MISSILE) {
+						projPtr.Draw(MissileTexture);
+					}
+					else {
+						projPtr.Draw();
+					}
+				}
+				for (const auto& astPtr : asteroids) {
+					astPtr->Draw();
+				}
+			}
+
+			player->Draw();
+
+			Renderer::Instance().End();
+		}
+		UnloadTexture(MissileTexture);
+	}
+	
+
+
+private:
+	Application()
+	{
+		asteroids.reserve(1000);
+		projectiles.reserve(10'000);
+	};
+
+	std::vector<std::unique_ptr<Asteroid>> asteroids;
+	std::vector<Projectile> projectiles;
+
+	Texture2D MissileTexture;
+
+	AsteroidShape currentShape = AsteroidShape::RANDOM;
+
+	static constexpr int C_WIDTH = 1600;
+	static constexpr int C_HEIGHT = 1400;
+	static constexpr size_t MAX_AST = 150;
+	static constexpr float C_SPAWN_MIN = 0.5f;
+	static constexpr float C_SPAWN_MAX = 3.0f;
+
+	static constexpr int C_MAX_ASTEROIDS = 1000;
+	static constexpr int C_MAX_PROJECTILES = 10'000;
+};
+
+int main() {
+	Application::Instance().Run();
+	return 0;
+}
